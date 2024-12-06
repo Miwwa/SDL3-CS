3c8243d8c31b910d60ecfe5002db47eb24a956ba
Mikhail <eremin.ms92@gmail.com>
2024-12-06 09:51:24 +0200

    Redirect standard output to standard error in entrypoint.sh
    This change modifies the script to redirect standard output to standard error for the GenerateBindings command. It ensures better error handling by displaying messages through the error stream, which can be useful for logging and debugging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "Generating ffi.json"
 /c2ffi/build/bin/c2ffi -i ./SDL/include -o ./GenerateBindings/assets/ffi.json ./GenerateBindings/assets/ffi.h
 
 echo "Generating SDL3.Legacy.cs"
-/GenerateBindings/bin/Release/net8.0/publish/GenerateBindings ./SDL
+/GenerateBindings/bin/Release/net8.0/publish/GenerateBindings ./SDL >&2
 echo "Generating SDL3.Core.cs"
-/GenerateBindings/bin/Release/net8.0/publish/GenerateBindings ./SDL --core
+/GenerateBindings/bin/Release/net8.0/publish/GenerateBindings ./SDL --core >&2
